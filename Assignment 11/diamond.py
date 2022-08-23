dae12a8b50daf607068fc71f3bcd3de528831663
Ask1519 <alivalizade@gmail.com>
2022-08-23 23:23:42 +0430

    Update Assignment 11

--- Assignment 11/diamond.py
+++ Assignment 11/diamond.py
@@ -1,4 +1,5 @@
 import arcade
+from PIL import Image
 
 BOARD_WIDTH = 500
 BOARD_HEIGHT = 500
@@ -19,23 +20,23 @@ class Diamond(arcade.Sprite):
         self.center_y = 0
         self.color1 = arcade.color.RED
         self.color2 = arcade.color.BLUE
+        self.angle = 45
 
     def draw(self):
-        count = 0
         for i in range(10):
             for j in range(10):
                 self.center_x = i * 30 + 120
                 self.center_y = j * 30 + 120
                 if i % 2 == 0:
                     if j % 2 != 0:
-                        arcade.draw_circle_filled(self.center_x, self.center_y, 10, self.color1)
+                        arcade.draw_rectangle_filled(self.center_x, self.center_y, 18, 18, self.color1, self.angle)
                     else:
-                        arcade.draw_circle_filled(self.center_x, self.center_y, 10, self.color2)
+                        arcade.draw_rectangle_filled(self.center_x, self.center_y, 18, 18, self.color2, self.angle)
                 else:
                     if j % 2 == 0:
-                        arcade.draw_circle_filled(self.center_x, self.center_y, 10, self.color1)
+                        arcade.draw_rectangle_filled(self.center_x, self.center_y, 18, 18, self.color1, self.angle)
                     else:
-                        arcade.draw_circle_filled(self.center_x, self.center_y, 10, self.color2)
+                        arcade.draw_rectangle_filled(self.center_x, self.center_y, 18, 18, self.color2, self.angle)
         
 board = Board()
 arcade.run()
